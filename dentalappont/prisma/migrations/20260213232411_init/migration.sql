99f6fc5afa9decda2f5f3076038d04a99450ac5d
Ayush Maurya <ayushmaurya2112@gmail.com>
2026-02-14 05:04:32 +0530

    Setup Prisma with PostgreSQL and initial database models

---
+++ dentalappont/prisma/migrations/20260213232411_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Appointment" (
+    "id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ dentalappont/src/app/api/test/route.ts
@@ -0,0 +1,7 @@
+// src/app/api/test/route.ts
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return Response.json(users);
+}
